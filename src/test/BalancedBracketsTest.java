23fc1d3e80deb1ce1a440f25d712e8576ddd79a5
Michael Watson <jmwatson2357@gmail.com>
2021-01-31 11:28:51 -0600

    added test ideas for studio discussion

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -12,5 +12,21 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    //    @Test
+//    public void onlyBracketsReturnsTrue() {
+//        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+//    }
+
+//  create an @Before to extract "[" and "]" into an array
+
+//      2. verify that "[" is always an even index
+//      3. verify that "]" is always an odd index;
+//      4. return fail statement if method returns a negative number
+//      5. return fail statement if method returns a positive number
+//    6. verify string has at least one "[" bracket
+//    7. verify string has at least one "]" bracket
+//    8. return fail statement if method does not have any "[" brackets
+//    9. return fail statement if method does not have any "]" brackets
+//    10. return success statement if method returns "0"
 
 }
